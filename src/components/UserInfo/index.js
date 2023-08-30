7194685ceaa763149aa6f2f3f11947edfc7ce3f6
venkateshhl1995 <venkateshhl1995@gmail.com>
2023-08-30 10:28:10 +0000

    solution

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1>Victory Venkatesh</h1>
+    <p>Software developer at Kolar</p>
+  </div>
+)
+
+export default UserInfo
